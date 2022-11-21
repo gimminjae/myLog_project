85c2f4e1386617fcd7adfc711769cd9c6397085b
gimminjae <min356812@naver.com>
2022-11-21 17:34:55 +0900

    Feat: 글 상세페이지 구현

--- src/main/java/com/mylog/post/controller/PostController.java
+++ src/main/java/com/mylog/post/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.mylog.post.controller;
 
+import com.mylog.base.util.Ut;
 import com.mylog.post.dto.PostDto;
 import com.mylog.post.form.PostForm;
 import com.mylog.post.service.PostService;
@@ -19,6 +20,7 @@ import java.util.List;
 public class PostController {
     private final PostService postService;
 
+    //글 목록, 메인 페이지, 시작 페이지
     @GetMapping("/list")
     public String postList(Model model) {
         List<PostDto> postDtos = postService.getAll();
@@ -26,17 +28,30 @@ public class PostController {
         model.addAttribute("postList", postDtos);
         return "post/list";
     }
+
+    //글 작성 폼
     @GetMapping("/write")
     public String postWrite(PostForm postForm) {
 
         return "post/write";
     }
+
+    //글 작성 처리
     @PostMapping("/write")
-    @ResponseBody
     public String postWrite(@Valid PostForm postForm, BindingResult bindingResult) {
 
-        postService.create(postForm.getSubject(), postForm.getContent());
+        PostDto postDto = postService.create(postForm.getSubject(), postForm.getContent());
 
-        return "success";
+        return "redirect:/post/%d?msg=%s".formatted(postDto.getId(), Ut.url.encode("글이 작성되었습니다!"));
+    }
+
+    //글 상세 페이지
+    @GetMapping("/{id}")
+    public String postDetail(@PathVariable("id") long id, Model model) {
+        PostDto postDto = postService.getById(id);
+
+        model.addAttribute("post", postDto);
+
+        return "post/detail";
     }
 }

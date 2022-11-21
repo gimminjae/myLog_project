5f01d5c41167f6a9a384a9c47881f4f2c19d1f74
gimminjae <min356812@naver.com>
2022-11-21 16:56:36 +0900

    git test

--- src/main/java/com/mylog/post/controller/PostController.java
+++ src/main/java/com/mylog/post/controller/PostController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.util.List;
-
+//git test
 @Controller
 @RequestMapping("/post")
 @RequiredArgsConstructor

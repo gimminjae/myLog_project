3ff6a37aff05fad2cb5ad433515116966aac00bf
gimminjae <min356812@naver.com>
2022-11-21 13:23:56 +0900

    Design: layout도입, navbar구현, postlist 구현

--- src/main/java/com/mylog/post/controller/PostController.java
+++ src/main/java/com/mylog/post/controller/PostController.java
@@ -1,15 +1,27 @@
 package com.mylog.post.controller;
 
+import com.mylog.post.dto.PostDto;
 import com.mylog.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/post")
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
+
+    @GetMapping("/list")
+    public String postList(Model model) {
+        List<PostDto> postDtos = postService.getAll();
+
+        model.addAttribute("postList", postDtos);
+        return "post/list";
+    }
 }

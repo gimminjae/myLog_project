1783d41d6410874cbe08466be927c87c2dd37390
gimminjae <min356812@naver.com>
2022-11-21 13:35:08 +0900

    Feat: application-dev.yml 생성, devinitdata도입

--- src/main/java/com/mylog/base/initData/TestInitData.java
+++ src/main/java/com/mylog/base/initData/TestInitData.java
@@ -1,17 +1,22 @@
 package com.mylog.base.initData;
 
+import com.mylog.post.service.PostService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test")
+@Profile({"dev", "test"})
 public class TestInitData {
     @Bean
-    CommandLineRunner init() {
+    CommandLineRunner init(
+            PostService postService
+    ) {
         return args -> {
-
+            postService.create("subject1", "content1");
+            postService.create("subject2", "content2");
+            postService.create("subject3", "content3");
         };
     }
 }

c3132dedca030945c7fbd6a4921cda6dd2ae48e9
gimminjae <min356812@naver.com>
2022-11-22 12:50:48 +0900

    Feat: MemberDto 생성, 회원생성 테스트 추가

---
+++ src/main/java/com/mylog/member/dto/MemberDto.java
@@ -0,0 +1,22 @@
+package com.mylog.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDto {
+    private long id;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+    private String username;
+    private String email;
+    private String nickname;
+//    private String password;
+}
--- src/test/java/com/mylog/member/service/MemberServiceTests.java
+++ src/test/java/com/mylog/member/service/MemberServiceTests.java
@@ -1,8 +1,50 @@
 package com.mylog.member.service;
 
+import com.mylog.member.dto.MemberDto;
+import com.mylog.post.service.PostService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class MemberServiceTests {
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private PlatformTransactionManager transactionManager;
 
+    TransactionStatus status;
+
+    @BeforeEach
+    void beforeEach() {
+        // 트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+
+    @AfterEach
+    void afterEach() {
+        // 트랜잭션 롤백
+        transactionManager.rollback(status);
+    }
+    @Test
+    @DisplayName("회원 생성")
+    void test1() {
+        String username = "user1";
+        String password = "user1";
+        String email = "test12@test.com";
+        String nickname = "user1";
+
+        MemberDto memberDto = memberService.create(username, password, email, nickname);
+
+        List<MemberDto> memberDtoList = memberService.getAll();
+        assertThat(memberDtoList.size()).isEqualTo(1);
+    }
 }

e1beae2fb7cb27f7ee30935f8c599303061804ec
gimminjae <min356812@naver.com>
2022-11-22 13:37:55 +0900

    Feat: securityConfig 로그인, 로그아웃 설정 추가

--- src/main/java/com/mylog/config/SecurityConfig.java
+++ src/main/java/com/mylog/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -14,6 +15,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/member/login") //로그인 url
+                .defaultSuccessUrl("/") //로그인 성공시 이동할 url
+                .and()
+                .logout()
+                //로그아웃 url
+                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                .logoutSuccessUrl("/") //로그아웃 성공시 이동할 url
+                .invalidateHttpSession(true) //로그아웃시 생성된 세션 삭제 활성화
         ;
         return http.build();
     }

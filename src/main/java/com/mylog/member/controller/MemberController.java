c992e55340b9e595d095490cdfafe361380c7c34
gimminjae <min356812@naver.com>
2022-11-23 17:48:41 +0900

    Style: MemberController 주석 추가

--- src/main/java/com/mylog/member/controller/MemberController.java
+++ src/main/java/com/mylog/member/controller/MemberController.java
@@ -20,36 +20,48 @@ import javax.validation.Valid;
 public class MemberController {
     private final MemberService memberService;
 
+    //로그인 폼
     @GetMapping("/login")
     public String login() {
         return "member/login";
     }
 
+    //회원가입 폼
     @GetMapping("/join")
     public String join(MemberForm memberForm) {
         return "member/join_form";
     }
+
+    //회원가입 처리
     @PostMapping("/join")
     public String join(@Valid MemberForm memberForm, BindingResult bindingResult) {
+        //빈 항목이 있을 경우
         if(bindingResult.hasErrors()) {
             bindingResult.reject("joinFailed", "빈 항목을 입력하세요");
             return "member/join_form";
         }
+
+        //2개의 비밀번호가 일치하지 않을 경우
         if(!memberForm.getPassword1().equals(memberForm.getPassword2())) {
             bindingResult.rejectValue("password2", "passwordIncorrect", "2개의 패스워드가 일치하지 않습니다.");
             return "member/join_form";
         }
         try {
+            //회원가입
             memberService.create(memberForm.getUsername(), memberForm.getPassword1(), memberForm.getEmail(), memberForm.getNickname());
         } catch(DataIntegrityViolationException e) {
+            //정보가 중복될 경우
             e.printStackTrace();
             bindingResult.reject("joinFailed", "이미 등록된 회원입니다.");
             return "member/join_form";
         } catch(Exception e) {
+            //기타 에러
             e.printStackTrace();
             bindingResult.reject("joinFailed", e.getMessage());
             return "member/join_form";
         }
+
+        //회원가입 완료
         return "redirect:/member/login?msg=%s".formatted(Ut.url.encode("회원가입 완료! 로그인하세요!"));
     }
 }

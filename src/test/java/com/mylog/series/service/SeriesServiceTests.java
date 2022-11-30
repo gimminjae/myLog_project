913ee9a61307a163cbe8ca0f6e15b8ca35fa5050
gimminjae <min356812@naver.com>
2022-11-30 12:42:38 +0900

    Test: SeriesServiceTests 생성

---
+++ src/test/java/com/mylog/series/service/SeriesServiceTests.java
@@ -0,0 +1,33 @@
+package com.mylog.series.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+@SpringBootTest
+public class SeriesServiceTests {
+    @Autowired
+    private SeriesService seriesService;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+
+    TransactionStatus status;
+
+    @BeforeEach
+    void beforeEach() {
+        // 트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+
+    @AfterEach
+    void afterEach() {
+        // 트랜잭션 롤백
+        transactionManager.rollback(status);
+    }
+}
